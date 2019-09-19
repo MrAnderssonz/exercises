a97ec702e9e4f62cc7088e9b0228d6ccd30776e6
Johan Andersson <ewan.andersson@gmail.com>
2019-09-19 14:10:08 +0200

    Övning 2 avklarad

---
+++ exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("skriv in ett decimaltal: ");
+            string input = Console.ReadLine();
+            double userInput = Convert.ToDouble(input);
+            int noDecimals = (int)userInput;
+            Console.WriteLine("Ditt tal {0 } utan decimaler är: {1}", userInput, noDecimals);
+
+        }
+    }
+}
